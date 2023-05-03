6c802dc8138f552b45adafac0661b64c9167f01a
deepanshudua555 <deepanshudua555@gmail.com.com>
2023-05-03 23:32:42 +0530

    login and signup

---
+++ navigation/appNavigation.js
@@ -0,0 +1,28 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+import WelcomeScreen from '../screens/WelcomeScreen';
+import LoginScreen from '../screens/LoginScreen';
+import SignUpTeacherScreen from '../screens/SignUpTeacherScreen';
+import SignUpForScreen from '../screens/SignUpForScreen';
+import SignUpStudentScreen from '../screens/SignUpStudentScreen';
+
+const Stack = createNativeStackNavigator();
+
+
+export default function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Welcome'>
+        <Stack.Screen name="Home" options={{headerShown: false}} component={HomeScreen} />
+        <Stack.Screen name="Welcome" options={{headerShown: false}} component={WelcomeScreen} />
+        <Stack.Screen name="Login" options={{headerShown: false}} component={LoginScreen} />
+        <Stack.Screen name="SignUpFor" options={{headerShown: false}} component={SignUpForScreen} />
+        <Stack.Screen name="SignUpTeacher" options={{headerShown: false}} component={SignUpTeacherScreen} />
+        <Stack.Screen name="SignUpStudent" options={{headerShown: false}} component={SignUpStudentScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
